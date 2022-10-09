cd43259be23ffad26d3e6e418b8048869f399274
FASS <j3kawesome@yandex.ru>
2022-10-09 18:47:51 +0300

    Fixed bug with init. classes

--- Program.cs
+++ Program.cs
@@ -7,22 +7,12 @@ namespace OOP_2
 {
     public class Rectangle
     {
-        public int a
-        {
-            get { return a; }
-            set { a = value; }  
-        }
-        public int b
-        {
-            get { return b; }
-            set { b = value; }
-        }
-        public int s
-        {
-            get { return s; }
-            set { s = value; }
-        }
-       public Rectangle(int a, int b, int s)
+        public int a { get; set; } // Автоматические свойства
+        public int b { get; set; }
+        public int s { get; set; }
+
+        public Rectangle() : this(0, 0, 0) { }    // Конструктор по умолчанию, если его не добавлять, то придется постоянно вводить параметры при создании экземпляра данного класса
+        public Rectangle(int a, int b, int s)
         {
             this.a = a;
             this.b = b;
@@ -37,6 +27,12 @@ namespace OOP_2
             int n = int.Parse(Console.ReadLine());
             Console.WriteLine();   
             Rectangle[] new_obj = new Rectangle[n];
+
+            for(int i = 0; i< n; i++)           // Добавлен цикл с инициализацией массива new_obj
+            {                                   // Дело в том, что создавая массив объектов, в нем все элементы инизиализированны null
+                new_obj[i] = new Rectangle();   // Создаем экземпляр класса с параметрами по умолчанию (конструктор, строка 14)
+            }
+
             for(int i = 0; i < n; i++)
             {
                 Console.Write("Сторона а = ");
